4a69a1f090299a1a4fb994a91e8d02ff9b65dba8
bangadam <bangadam.dev@gmail.com>
2018-01-28 18:58:00 +0700

    update

--- database/migrations/2017_12_31_075316_SubDetailTransaksi.php
+++ database/migrations/2017_12_31_075316_SubDetailTransaksi.php
@@ -17,9 +17,9 @@ class SubDetailTransaksi extends Migration
             $table->increments('kode_sub', 8);
             $table->integer('kode_detail');
             $table->string('nama_bahan', 25);
-            $table->string('gambar_produk');
-            $table->string('gambar_logo');
-            $table->string('gambar_sendiri');
+            $table->string('gambar_produk')->nullable();
+            $table->string('gambar_logo')->nullable();
+            $table->string('gambar_sendiri')->nullable();
             $table->integer('jumlah');
             $table->integer('subtotal');
             $table->timestamps();
